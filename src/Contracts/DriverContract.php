fdc9b0e5d89034d6083848fc918ce0bae531bbd8
miladimos <milad.jafari6210@gmail.com>
2021-08-28 18:31:14 -0700

    add Driver

---
+++ src/Contracts/DriverContract.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Laravelir\Newsletters\Contracts;
+
+interface DriverContract
+{
+    public function subscribe();
+
+    public function unsubscribe();
+
+    public function toggleSubscribe();
+
+    public function deleteSubscriber();
+}
---
+++ src/Drivers/Driver.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Laravelir\Newsletters\Drivers;
+
+use Laravelir\Newsletters\Contracts\DriverContract;
+
+abstract class Driver implements DriverContract
+{
+    //
+}
